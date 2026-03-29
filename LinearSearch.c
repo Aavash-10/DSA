1bdb7cbb9f587a58a98736dd646a35118fcdc09d
Aavash-10 <sirshrestha001@gmail.com>
2026-03-29 08:18:00 +0545

    Add files via upload

---
+++ LinearSearch.c
@@ -0,0 +1,27 @@
+//Linear Search
+#include<stdio.h>
+
+int linearSearch(int arr[], int n, int key){
+	int i;
+
+        for(i = 0; i<n; i++){
+		if(arr[i] == key){
+			return i;
+			}
+	}
+	return -1;
+}
+int main(){
+	int arr[]={10,20,30,40,50};
+	int n= 5;
+	int key=50;
+
+	int result = linearSearch(arr,n,key);
+	
+	if (result!=-1)
+		printf("Element found at index %d\n", result);
+	else
+		printf("Element not found\n");
+
+	return 0;
+}
